a7aaca03cb8763e5c832ef8729274ff45ac54efd
BraisGarciaPol <a24braisgp@iessanclemente.net>
2024-10-07 20:11:19 +0200

    comezo proxect

---
+++ www/proxecto/style.css
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+ <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  
+
+  <div class="logindiv">
+    <a href="login_user.php">Login</a>
+  </div>
+  <div class="creatediv">
+    <a href="create_char.php">Create a character</a>
+  </div>
+  <div class="rolldiv">
+    <a href="roll_dice.php">Roll the dice</a>
+  </div>
+
+
+<footer>
+  <div class="logout">
+    <a href="logout.php">Logout</a>
+  </div>
+</footer>
+
+</body>
+</html>
