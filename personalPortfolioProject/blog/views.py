51e9e8db1c55b47d25a7b9ed3564eb10ba1660c1
A24BraisGP <a24braisgp@iessanclemente.net>
2024-11-14 14:24:05 +0100

    blog projects

--- personalPortfolioProject/blog/views.py
+++ personalPortfolioProject/blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+from .models import Blog
 # Create your views here.
 def all_blogs(request):
-    return render(request,'blog/all_blogs.html')
+    blogs = Blog.objects.all()
+    
+    # if request.GET.get('order'):
+    #     blogs = Blog.objects.order_by('data').all()
+    
+    return render(request,'blog/all_blogs.html',{'blogs':blogs})
