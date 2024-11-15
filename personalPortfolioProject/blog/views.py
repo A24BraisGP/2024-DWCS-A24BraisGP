55e45766120cc368739d1bc83f6be3ddc3be4235
A24BraisGP <a24braisgp@iessanclemente.net>
2024-11-15 11:26:17 +0100

    a

--- personalPortfolioProject/blog/views.py
+++ personalPortfolioProject/blog/views.py
@@ -4,7 +4,4 @@ from .models import Blog
 def all_blogs(request):
     blogs = Blog.objects.all()
     
-    # if request.GET.get('order'):
-    #     blogs = Blog.objects.order_by('data').all()
-    
     return render(request,'blog/all_blogs.html',{'blogs':blogs})
