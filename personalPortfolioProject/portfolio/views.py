fc9528119fd17842643577b85049636859d8b812
a24braisgp <a24braisgp@iessanclemente.net>
2024-11-17 20:01:29 +0100

    a

--- personalPortfolioProject/portfolio/views.py
+++ personalPortfolioProject/portfolio/views.py
@@ -6,4 +6,5 @@ from .models import Project
 # Create your views here.
 def home(request):
     projects = Project.objects.all()
+    
     return render(request,'portfolio/home.html',{'projects':projects})
